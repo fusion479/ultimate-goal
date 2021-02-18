af84ffcb659d58ca7251c26c58eaefc0afe4826e
turtlelazy <ishraqmahid@gmail.com>
2021-02-17 19:54:39 -0500

    k values edit

--- drive/DriveConstants.java
+++ drive/DriveConstants.java
@@ -55,9 +55,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.019;
-    public static double kA = 0.005;
-    public static double kStatic = 0;
+    public static double kV = 0.0176;
+    public static double kA = 0.0039;
+    public static double kStatic = 0.06;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
