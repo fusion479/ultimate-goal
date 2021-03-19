4dc5bf0c0e2330ec8e027d941736f0d0ec8d6f2e
Ishraq Mahid <ishraqmahid@gmail.com>
2021-03-19 01:03:25 -0400

    intake bug fix

--- hardware/CompleteIntake.java
+++ hardware/CompleteIntake.java
@@ -12,33 +12,19 @@ public class CompleteIntake extends  Mechanism{
         acquirerM = hwMap.dcMotor.get("acquirerM");
         acquirerM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         acquirerM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        acquirerM.setDirection(DcMotorSimple.Direction.FORWARD);
 
         indexerM = hwMap.dcMotor.get("indexer");
         indexerM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         indexerM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        indexerM.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     public void intake(float power){
-        indexerM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        acquirerM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        acquirerM.setDirection(DcMotorSimple.Direction.FORWARD);
         acquirerM.setPower(power);
-
-        indexerM.setDirection(DcMotorSimple.Direction.REVERSE);
         indexerM.setPower(power);
     }
 
     public void outake(float power){
-        indexerM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        acquirerM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        acquirerM.setDirection(DcMotorSimple.Direction.REVERSE);
         acquirerM.setPower(power);
-
-        indexerM.setDirection(DcMotorSimple.Direction.FORWARD);
         indexerM.setPower(power);
     }
 
